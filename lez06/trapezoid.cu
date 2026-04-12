5b6defd0839578772afc2b784825954afc1e0315
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2026-04-12 21:31:36 +0200

    refactored to work without knowing shared memory size

--- lez06/trapezoid.cu
+++ lez06/trapezoid.cu
@@ -5,14 +5,23 @@
 #include <omp.h>
 
 /**
+Note for shared memory:
+We should get the best performance if sdata is declared in the kernel as
+  extern __shared__ double sdata[]
+We want the shared memory size to be equal to the block size
+  shMem = blockSize * sizeof(double)
+And the kernel must be called with 3 arguments in the triple angle brackets
+  <<<Nblocks, blockSize, shMem>>>
+*/
+
+/**
 Shared memory, tree-structured sum.
 Pair up the threads so that half of the “active” threads add their partial sum to their partner’s partial sum.
 */
-__device__ void shared_mem_tree_sum(double *sdata,
-                                    const unsigned int sdata_len) {
+__device__ void shared_mem_tree_sum(double sdata[]) {
   const unsigned int tid = threadIdx.x;
   for (unsigned int stride = (blockDim.x >> 1); stride > 0; stride >>= 1) {
-    if (tid < stride && tid + stride < sdata_len) {
+    if (tid < stride) {
       sdata[tid] += sdata[tid + stride];
     }
     __syncthreads();
@@ -37,9 +46,8 @@ __device__ double warp_shuffle_tree_sum(double val) {
 Every thread reads a value from another thread in each step.
 At the end, all the threads have the total sum.
 On each step, shift 'val' down by 'offset' lanes and add it to the local 'val’.
-Make sure 0 <= source <= warpSize.
-
 This function works if sdata is in shared or global memory.
+Make sure 0 <= source <= warpSize.
 */
 __device__ double shared_mem_dissemination_sum(double sdata[]) {
   const unsigned int tid = threadIdx.x;
@@ -75,17 +83,17 @@ __global__ void trap_gpu_naive(const double a, const unsigned long n,
 __global__ void trap_gpu_shared_mem_tree_sum(const double a,
                                              const unsigned long n,
                                              const double h, double *res) {
-  __shared__ double sdata[SHARED_MEM_SIZE];
+  extern __shared__ double sdata[];
   const unsigned int tid = threadIdx.x;
   const unsigned int i = threadIdx.x + blockIdx.x * blockDim.x;
 
-  if (tid <= SHARED_MEM_SIZE && i < n && i > 0) {
+  if (i < n && i > 0) {
     double x_i = a + i * h;
     sdata[tid] = f(x_i);
   }
   __syncthreads();
 
-  shared_mem_tree_sum(sdata, SHARED_MEM_SIZE);
+  shared_mem_tree_sum(sdata);
   if (tid == 0) {
     atomicAdd(res, sdata[0]);
   }
@@ -110,14 +118,6 @@ __global__ void trap_gpu_warp_shuffle_tree_sum(const double a,
   }
 }
 
-/**
-We should get the best performance if sdata is declared in the kernel as
-  extern __shared__ double sdata[]
-We want the shared memory size to be equal to the block size
-  shMem = blockSize * sizeof(double)
-And the kernel must be called with 3 arguments in the triple angle brackets
-  <<<Nblocks, blockSize, shMem>>>
-*/
 __global__ void trap_gpu_shared_mem_dissemination_sum(const double a,
                                                       const unsigned long n,
                                                       const double h,
