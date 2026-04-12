3d6835f4e69e73a44b3cfffe2f44e9a368c65203
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2026-04-12 21:31:17 +0200

    removed shared_mem_size constant

--- lez06/utils.hpp
+++ lez06/utils.hpp
@@ -3,7 +3,6 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#define SHARED_MEM_SIZE 256
 #define FULL_MASK 0xffffffff
 
 inline __device__ __host__ double f(const double x) { return 2*sin(x) + 3*pow(cos(x), 2) + 5*pow(x, 3); }
