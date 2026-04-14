7262fbe53ecab0497b6526302fde2b5bdced3d1a
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2026-04-15 00:22:51 +0200

    script produces another json for uneven runs

--- 03-trapezoid/run.sh
+++ 03-trapezoid/run.sh
@@ -4,12 +4,32 @@ if [ ! -f main.exe ]; then
     make -j
 fi
 
+blocksize_exp=1
+num_div_exp=20
+num_div_sub=0
+
 echo "{" > results.json
+
 for i in {1..10}; do
     echo "\"Test $i\": " >> results.json
-    ./main.exe >> results.json
+    ./main.exe $blocksize_exp $num_div_exp $num_div_sub >> results.json
     if [ $i -lt 10 ]; then
         echo "," >> results.json
     fi
 done
+
 echo "}" >> results.json
+
+num_div_sub=17
+
+echo "{" > results_uneven.json
+
+for i in {1..10}; do
+    echo "\"Test $i\": " >> results_uneven.json
+    ./main.exe $blocksize_exp $num_div_exp $num_div_sub >> results_uneven.json
+    if [ $i -lt 10 ]; then
+        echo "," >> results_uneven.json
+    fi
+done
+
+echo "}" >> results_uneven.json
